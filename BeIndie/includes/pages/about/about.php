7d4f7a6f71d0fd4469812e3777be9d371940c8fd
ro6520wa <ronwagner07@yahoo.de>
2017-01-10 12:53:04 +0100

    about with database conn

--- BeIndie/includes/pages/about/about.php
+++ BeIndie/includes/pages/about/about.php
@@ -1,4 +1,18 @@
-<h1>About</h1>
-<article>
-    Hier wird das Unternehmen vorgestellt.
-</article>
+<div id="wrapper">
+    <div id="content">
+        <h1>Über uns</h1>
+        <article>
+            <?php
+            include ("includes/functions/swConnect.php");
+            $query1 = "select * from employee";
+            $result1 = mysqli_query($conn, $query1);
+
+            while ($row = mysqli_fetch_array($result1)) 
+            {?>
+            <b>Name: </b> <?= $row["e_firstname"]. " ". $row["e_lastname"] .  "<br/>";?>
+            <b>Geburtsdatum</b> <?php echo$row["e_birthdate"] . "<br/>";
+            }?>
+            <?php include ("includes/functions/swClose.php"); ?>
+        </article>
+    </div>
+</div>
