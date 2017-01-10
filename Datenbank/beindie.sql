1ca8c2c27c161d5d4cf6790366607fa9c1c6579a
GormanNünther <hentgenpaul@yahoo.de>
2017-01-10 20:44:05 +0100

    DB Project images

--- Datenbank/beindie.sql
+++ Datenbank/beindie.sql
@@ -1,21 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Erstellungszeit: 10. Jan 2017 um 15:14
--- Server-Version: 10.1.16-MariaDB
--- PHP-Version: 5.6.24
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Datenbank: `beindie`
 --
@@ -39,7 +21,7 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`e_ID`, `e_firstname`, `e_lastname`, `e_birthdate`, `e_image`) VALUES
-(1, 'Ron', 'Wagner', '25.07.1996', 'images/e_images/ronw.jpg'),
+(1, 'Ran', 'Wogner', '25.07.1996', 'images/e_images/ronw.jpg'),
 (2, 'Paul', 'Hentgen', '25.08.1992', 'images/e_images/paulh.jpg');
 
 -- --------------------------------------------------------
@@ -52,7 +34,6 @@ CREATE TABLE `project` (
   `project_ID` int(11) NOT NULL,
   `creator` varchar(200) NOT NULL,
   `title` varchar(200) NOT NULL,
-  `picture` varchar(200) NOT NULL,
   `goal` int(11) NOT NULL,
   `current_status` int(11) NOT NULL,
   `start_date` date NOT NULL,
@@ -63,8 +44,49 @@ CREATE TABLE `project` (
 -- Daten für Tabelle `project`
 --
 
-INSERT INTO `project` (`project_ID`, `creator`, `title`, `picture`, `goal`, `current_status`, `start_date`, `end_date`) VALUES
-(2, 'John.D@gmail.com', 'Awesome Project', '/img/slideshow1.jpg', 500000, 125000, '2016-12-01', '2017-01-16');
+INSERT INTO `project` (`project_ID`, `creator`, `title`, `goal`, `current_status`, `start_date`, `end_date`) VALUES
+(2, 'John.D@gmail.com', 'Awesome Project', 500000, 125000, '2016-12-01', '2017-01-16');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `project_image`
+--
+
+CREATE TABLE `project_image` (
+  `image_ID` int(11) NOT NULL,
+  `project_ID` int(11) NOT NULL,
+  `slideshow_picture` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Daten für Tabelle `project_image`
+--
+
+INSERT INTO `project_image` (`image_ID`, `project_ID`, `slideshow_picture`) VALUES
+(1, 2, 'images/slideshow_img/awesome1.jpg'),
+(2, 2, 'images/slideshow_img/awesome2.jpg'),
+(3, 2, 'images/slideshow_img/awesome3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `ID` int(11) NOT NULL,
+  `user_ID` varchar(200) NOT NULL,
+  `project_ID` int(11) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Daten für Tabelle `transaction`
+--
+
+INSERT INTO `transaction` (`ID`, `user_ID`, `project_ID`, `amount`) VALUES
+(1, 'Not.Doe@gmail.com', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -107,6 +129,21 @@ ALTER TABLE `project`
   ADD KEY `creator` (`creator`);
 
 --
+-- Indizes für die Tabelle `project_image`
+--
+ALTER TABLE `project_image`
+  ADD PRIMARY KEY (`image_ID`),
+  ADD KEY `project_ID` (`project_ID`);
+
+--
+-- Indizes für die Tabelle `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `creator` (`user_ID`),
+  ADD KEY `project_ID` (`project_ID`);
+
+--
 -- Indizes für die Tabelle `user`
 --
 ALTER TABLE `user`
@@ -128,6 +165,16 @@ ALTER TABLE `employee`
 ALTER TABLE `project`
   MODIFY `project_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
+-- AUTO_INCREMENT für Tabelle `project_image`
+--
+ALTER TABLE `project_image`
+  MODIFY `image_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT für Tabelle `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT für Tabelle `user`
 --
 ALTER TABLE `user`
@@ -141,6 +188,20 @@ ALTER TABLE `user`
 --
 ALTER TABLE `project`
   ADD CONSTRAINT `project_ibfk_1` FOREIGN KEY (`creator`) REFERENCES `user` (`e-mail`);
+
+--
+-- Constraints der Tabelle `project_image`
+--
+ALTER TABLE `project_image`
+  ADD CONSTRAINT `project_image_ibfk_1` FOREIGN KEY (`project_ID`) REFERENCES `project` (`project_ID`);
+
+--
+-- Constraints der Tabelle `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`e-mail`),
+  ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`project_ID`) REFERENCES `project` (`project_ID`),
+  ADD CONSTRAINT `transaction_ibfk_3` FOREIGN KEY (`user_ID`) REFERENCES `user` (`e-mail`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
