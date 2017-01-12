2cd0975038037937a32d4b04b4de2087c296f3c4
GormanNünther <hentgenpaul@yahoo.de>
2017-01-12 17:35:48 +0100

    Project page

--- BeIndie/includes/pages/my_projects/my_projects.php
+++ BeIndie/includes/pages/my_projects/my_projects.php
@@ -1,44 +1,71 @@
-    
- 
-<section class="product">
-    <div class="product">
-        <h1 align="center">Projekttitel</h1>
-    </div>
-    <div id="slideshow">
-       
-    <img class="slideshow" src="images/slideshow1.jpg" >
-    <img class="slideshow" src="images/slideshow2.jpg" >
-    <img class="slideshow" src="images/slideshow3.jpg" >    
-    
-    </div>
-    
+   
 
-    <h2>3'951€</h2>
+<div class="projects">
+    <div id="project">
+            <h1 align="center">Projekttitel</h1>
+        <div id="slideshow">
 
-    <div id="progress">
-      <div id="progressbar">
-        <div id="label">82%</div>
-      </div>
+        <img class="slideshow" src="images/slideshow1.jpg" >
+        <img class="slideshow" src="images/slideshow2.jpg" >
+        <img class="slideshow" src="images/slideshow3.jpg" >    
+
+        <a class="left" onclick="plusDivs(-1)">&#10094;</a>
+        <a class="right" onclick="plusDivs(1)">&#10095;</a>
+
+        </div>
+
+        
+        <div id="progress">
+          <div id="progressbar">
+            <div id="label">80%</div>
+          </div>
+        </div>
+         
+        <div id="info"> 
+            <div id ="timetogo">
+                    <h2>27</h2>
+                    <p>Stunden bis zum Ziel</p>
+            </div>
+
+            <div id ="backers">
+                    <h2>10</h2>
+                    <p>Unterstützer</p>
+            </div>
+            
+            <div id="goal">   
+            <h2>8.000€ / 10.000€</h2>
+            <p>Fortschritt</p>
+            </div>
+        </div> 
+        <div id ="Text">
+            
+            <div id ="desciption">
+                <h2>Projektbeschreibung</h2>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.  
+                    Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.   
+                    Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.   
+                    Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.   
+                    Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.   
+                    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            </div>
+
+            <div id ="backing">
+                
+                <h2>Dieses Projekt unterstützen</h2>
+                <button type="button" >Unterstützen</button>
+                <h3>1€ aufwärts</h3>
+                <h4>Belohnung</h4>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy </p>   
+                <h3>5€ aufwärts</h3>
+                <h4>Belohnung</h4>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy </p>
+                <h3>10€ aufwärts</h3>
+                <h4>Belohnung</h4>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy </p>
+            </div>
+        </div>
     </div>
-    
-    
-    <div id ="desciption">
-        <h2>Projektbeschreibung</h2>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-    </div>
-    
-    <div id ="backing">
-        <h2>Dieses Projekt unterstützen</h2>
-        <h3>1€ aufwärts</h3>
-        <h4>Belohnung</h4>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy </p>   
-        <h3>5€ aufwärts</h3>
-        <h4>Belohnung</h4>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy </p>
-        <h3>10€ aufwärts</h3>
-        <h4>Belohnung</h4>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy </p>
-</section>
+</div>
 
 
 
@@ -58,5 +85,24 @@
         x[myIndex-1].style.display = "block";  
         setTimeout(carousel, 2000); 
     }   
+    
+    
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+      showDivs(slideIndex += n);
+    }
+
+    function showDivs(n) {
+      var i;
+      var x = document.getElementsByClassName("slideshow");
+      if (n > x.length) {slideIndex = 1}    
+      if (n < 1) {slideIndex = x.length}
+      for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none";  
+      }
+      x[slideIndex-1].style.display = "block";  
+    }
 </script>
 
