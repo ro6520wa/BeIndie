71f12faa3871d902e7fbd4ae673873063315c0fa
ro6520wa <ronwagner07@yahoo.de>
2017-01-22 01:05:13 +0100

    small profile pic delete fix

--- BeIndie/includes/functions/delete_uimg.php
+++ BeIndie/includes/functions/delete_uimg.php
@@ -9,6 +9,8 @@ $query = "UPDATE user SET avatar='' WHERE user_ID = '" . $uid . "'";
 $result = mysqli_query($conn, $query);
 
 if($result == true){
+    $output = mysqli_fetch_assoc($result);
+    unlink("../../" . $output["avatar"]);
     header('Location: ../../index.php?page=edit_profile&id=' . $uid . "&succ=1");
     exit();
 }
