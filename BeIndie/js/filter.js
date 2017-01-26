fb9fb59116c13431848cc71994ac7941db84c2ee
ro6520wa <ronwagner07@yahoo.de>
2017-01-26 16:12:17 +0100

    filter fix

--- BeIndie/js/filter.js
+++ BeIndie/js/filter.js
@@ -1,23 +1,13 @@
-//var acc = document.getElementsByClassName("filter");
-//var i;
 $(document).ready(function(){
     var clicks = 0;
     $(".filter").on("click" , function(){
         if(clicks === 0){
             $(".filter").addClass("active");
+            clicks++;
         }else{
             $(".filter").removeClass("active");
+            clicks--;
         }
         $(".filter_panel").toggle();
     })
 })
-//for (i = 0; i < acc.length; i++) {
-//    acc[i].onclick = function(){
-//        this.classList.toggle("active");
-//        this.nextElementSibling.classList.toggle("show");
-//    }
-//}
-
-
-
-
