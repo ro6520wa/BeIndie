4761c08bb43d9968bed58ef30f79cb6cc5725ad4
ro6520wa <ronwagner07@yahoo.de>
2017-02-25 21:18:11 +0100

    documentation and some minor fixes

--- BeIndie/includes/pages/documentation/documentation.php
+++ BeIndie/includes/pages/documentation/documentation.php
@@ -1,4 +1,354 @@
 <div id="docu_wrap">
-    Insert Doku here.
-</div>
+    <h1>Dokumentation</h1>
+    <h2>Gliederung</h2>
+    <ul>
+        <li>
+            <a href="#inhalt">1. Inhalt</a>
+            <ul>
+                <li><a href="#fik_kunde">1.1. Beschreibung des fiktiven Kunden und seiner Ziele</a></li>
+                <li><a href="#site_nutzer">1.2. Beschreibung der späteren Websitenutzer</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#rech_erg">2. Ergebnisse der Recherche</a>
+            <ul>
+                <li><a href="#kickstarter">2.1. Kickstarter</a> </li>
+                <li><a href="#indiegogo">2.2. Indiegogo</a> </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#layout">3. Wireframe und Sitemap</a>
+            <ul>
+                <li><a href="#wireframe">3.1. Wireframe</a></li>
+                <li><a href="#sitemap">3.2. Sitemap</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#design">4. Design</a>
+        </li>
+        <li>
+            <a href="#seite">5. Seitenbeschreibung</a>
+            <ul>
+                <li>
+                    <a href="#startseite">5.1. Startseite</a>
+                </li>
+                <li>
+                    <a href="#projekt_erstellen">5.2. Projekt erstellen</a>
+                </li>
+                <li>
+                    <a href="#projektseite">5.3. Projektseite</a>
+                </li>
+                <li>
+                    <a href="#projekt_unterst">5.4. Projekt unterstützen Seite</a>
+                </li>
+                <li>
+                    <a href="#nutzerprofil">5.5. Nutzerprofil</a>
+                </li>
+                <li>
+                    <a href="#profil_bearb">5.6. Profil bearbeiten</a>
+                </li>
+                <li>
+                    <a href="#meine_projekte">5.7. Meine Projekte</a>
+                </li>
+                <li>
+                    <a href="#ueber_uns">5.8. Über uns</a>
+                </li>
+                <li>
+                    <a href="#kontakt">5.9. Kontakt</a>
+                </li>
+                <li>
+                    <a href="#faq">5.10. FAQ</a>
+                </li>
+                <li>
+                    <a href="#impressum">5.11. Impressum</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#datenbank">6. Datenbank</a>
+            <ul>
+                <li>
+                    <a href="#er_modell">6.1. ER-Modell</a>
+                </li>
+                <li>
+                    <a href="#rel_modell">6.2. Relationales Modell</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#rollen">7. Rollenmodell</a>
+        </li>
+        <li>
+            <a href="#herausforderungen">8. Bewältigte Herausforderungen</a>
+        </li>
+        <li>
+            <a href="#besonderheiten">9. Besonderheiten für die Nutzung oder Bewertung</a>
+        </li>
+        <li>
+            <a href="#projekt_man">10. Projektmanagement-Dokumentation</a>
+        </li>
+    </ul>
+    <div id="docu_content">
+        <div id="inhalt">
+            <h2>1. Inhalt</h2>
+            <div id="fik_kunde">
+                <h3>1.1. Beschreibung des fiktiven Kunden und seiner Ziele</h3>
+                <p>Unser fiktive Kunde ist eine Crowdfunding Firma. Diese Firma heißt BeIndie. Das Ziel von BeIndie ist es jedem der ein Projekt umsetzen möchte, dem
+                aber die geldlichen Mittel dazu fehlen dies umzusetzen.</p>
+            </div>
+            <div id="site_nutzer">
+                <h3>1.2. Beschreibung des fiktiven Kunden und seiner Ziele</h3>
+                <p>Der spätere Nutzer kann sich einen Account auf der Seite erstellen um Projekte zu unterstützen oder selbst Projekte zu erstellen.
+                Dabei erhält er ein eigenes Profil, welches er mit einem Profilbild und einer eigenen Beschreibung gestalten kann. Außerdem kann
+                man auf dem Nutzerprofil die Projekte des Nutzer und Projekte die der Nutzer unterstützt einsehen. Ohne Account kann man die Suchfunktion
+                der Seite benutzen sowie sich Profile und und Projekte anschauen. Man kann sich jedoch nicht Einloggen, Projekte unterstützen oder selbst
+                Projekte erstellen.</p>
+            </div>
+        </div>
+        <div id="rech_erg">
+            <h2>2. Ergebnisse der Recherche</h2>
+            <div id="kickstarter">
+                <h3>2.1. Kickstarter</h3>
+                <p><b>Startseite</b></p>
+                <img src="dokumentation/kickstarter_start.jpg" alt="kickstarter_start" width="980">
+                <p><b>Projektseite</b></p>
+                <img src="dokumentation/kickstarter_project.jpg" alt="kickstarter_project" width="980">
+                <p><b>Projekt Unterstützen Seite</b></p>
+                <img src="dokumentation/kickstarter_support.png" alt="kickstarter_support" width="980">
+            </div>
+            <div id="indiegogo">
+                <h3>2.2. Indiegogo</h3>
+                <p><b>Startseite</b></p>
+                <img src="dokumentation/indiegogo_start.jpg" alt="indiegogo_start" width="980">
+                <p><b>Projektseite</b></p>
+                <img src="dokumentation/indiegogo_project.jpg" alt="indiegogo_project" width="980">
+                <p><b>Projekt Unterstützen Seite</b></p>
+                <img src="dokumentation/indiegogo_support.png" alt="indiegogo_support" width="980">
+            </div>
+        </div>
+        <div id="layout">
+            <h2>3. Wireframe und Sitemap</h2>
+            <div id="wireframe">
+                <h3>3.1. Wireframe</h3>
+                <p><b>Startseite</b></p>
+                <img src="dokumentation/beindie_start_mock.png" alt="beindie_start_mock" width="980">
+                <p><b>Projektseite</b></p>
+                <img src="dokumentation/beindie_project_mock.png" alt="beindie_project_mock" width="980">
+                <p><b>Projekt Unterstützen Seite</b></p>
+                <img src="dokumentation/beindie_support_mock.png" alt="beindie_support_mock" width="980">
+                <p><b>Profil</b></p>
+                <img src="dokumentation/beindie_profile_mock.png" alt="beindie_profile_mock" width="980">
+            </div>
+            <div id="sitemap">
+                <h3>3.2. Sitemap</h3>
+                <img src="dokumentation/sitemap.png" alt="sitemap" width="980">
+            </div>
+        </div>
+        <div id="design">
+            <h2>4. Design</h2>
+            <p>Beim Layout haben wir uns für große Bilder und wenig Text auf den wichtigen Seiten entschieden. Natürlich sind manche Seiten
+            sehr durch den Nutzer bestimmt. Deshalb können dort größere Texte auftreten. Außerdem haben wir Wert auf eine gute Lesbarkeit gelegt
+            und deshalb Abstände immer relativ groß gehalten. Außerdem war es uns wichtig immer und überall die wichtigsten Informationen zu sehen.
+            Aus diesem Grund erhält man z.B. so viele Daten zu den Projekten wie möglich, ohne das die Seite überladen wirkt. Desweiteren war es uns
+            wichtig, dass die Navbar am Kopf der Seite fixiert ist. Da bei der Suche beispielsweise eine relativ lange Seite entstehen könnte, war
+            es uns wichtig die wichtigsten Seite immer erreichbar zu haben. Die Farben sind auf allen Seiten einheitlich. Eine Grün/Grau/Hellgrau/Weiß Kombination
+            erschien uns als sehr minimalistisch, aber dennoch modern und einladend. Bei der Schriftart haben wir uns einheitlich für die Schriftart
+            "Raleway" ohne Serifen entschieden. Die Größen der jeweiligen Header und der Schrift ist nach eigenem Ermessen gewählt doch auch hier
+            haben wir wert auf gute Lesbarkeit und Modernität gesetzt. Eine Schriftart mit Serifen erschien uns dabei z.B. etwas altbacken.</p>
+        </div>
+        <div id="seite">
+            <h2>5. Seitenbeschreibung</h2>
+            <div id="startseite">
+                <h3>5.1. Startseite</h3>
+                <img src="dokumentation/beindie_start.jpg" alt="beindie_start" width="980">
+            </div>
+            <div id="projekt_erstellen">
+                <h3>5.2. Projekt erstellen</h3>
+                <!--<img src="dokumentation/beindie_start.jpg" alt="beindie_start" width="600">-->
+            </div>
+            <div id="projektseite">
+                <h3>5.3. Projektseite</h3>
+                <img src="dokumentation/beindie_project.jpg" alt="beindie_project" width="980">
+            </div>
+            <div id="projekt_unterst">
+                <h3>5.4. Projekt unterstützen Seite</h3>
+                <img src="dokumentation/beindie_support.jpg" alt="beindie_support" width="980">
+            </div>
+            <div id="nutzerprofil">
+                <h3>5.5. Nutzerprofil</h3>
+                <img src="dokumentation/beindie_profile.jpg" alt="beindie_profile" width="980">
+            </div>
+            <div id="profil_bearb">
+                <h3>5.6. Profil bearbeiten</h3>
+                <img src="dokumentation/beindie_edit_profile.jpg" alt="beindie_edit_profile" width="980">
+            </div>
+            <div id="meine_projekte">
+                <h3>5.7. Meine Projekte</h3>
+                <img src="dokumentation/beindie_my_projects.png" alt="beindie_my_projects" width="980">
+            </div>
+            <div id="ueber_uns">
+                <h3>5.8. Über Uns</h3>
+                <img src="dokumentation/beindie_about.png" alt="beindie_about" width="980">
+            </div>
+            <div id="kontakt">
+                <h3>5.9. Kontakt</h3>
+                <img src="dokumentation/beindie_contact.png" alt="beindie_contact" width="980">
+            </div>
+            <div id="faq">
+                <h3>5.10. FAQ</h3>
+                <img src="dokumentation/beindie_faq.png" alt="beindie_faq" width="980">
+            </div>
+            <div id="impressum">
+                <h3>5.11. Impressum</h3>
+                <img src="dokumentation/beindie_imprint.png" alt="beindie_imprint" width="980">
+            </div>
+        </div>
+        <div id="datenbank">
+            <h2>6. Datenbank</h2>
+            <div id="er_modell">
+                <h3>6.1. ER-Modell</h3>
+            </div>
+            <div id="rel_modell">
+                <h3>6.2. Relationales Modell</h3>
+            </div>
+        </div>
+        <div id="rollen">
+            <h2>7. Rollenmodell</h2>
+            <p>Es gibt eingeloggte Nutzer und nicht eingeloggte Nutzer. Desweiteren Admins, welche über die php-myadmin Oberfläche vollen Zugriff auf
+            die Datenbank haben. Eingeloggte Nutzer können alle Funktionen der Seite uneingeschränkt Nutzen. Nicht eingeloggte Nutzer können
+            sich Registrieren, allerdings weder Projekte erstellen, noch Projekte unterstützen.</p>
+        </div>
+        <div id="herausforderungen">
+            <h2>8. Bewältigte Herausforderungen</h2>
+            <h3>Fixed Navbar</h3>
+            <p><b>Problem:</b> Die Navbar sollte nach dem scrollen zu einem gewissen Punkt oben am Bildschirm "kleben" bleiben.</p>
+            <p><b>Lösung:</b>jQuery Code, welcher die Scrollposition überprüft.</p>
+            <h3>Dynamische Suche/Projekt unterstützen</h3>
+            <p><b>Problem:</b> Die Suche sollte sich dynamisch bei Nutzereingabe durch Textinput oder Filterveränderung</p>
+            <p><b>Lösung:</b> Via jQuery und Ajax wird bei Nutzereingaben der HTML Code in einer php-Funktion erzeugt.</p>
+            <p><b>Problem:</b> Beim Eintragen von Umlauten (ä,ö,ü,ß) in die Datenbank (via Profil
+            bearbeiten, Registrieren, Projekt erstellen/bearbeiten), werden diese nicht richtig erkannt und</p>
+            <p><b>Lösung:</b> Setzen der Datenbankkollation auf utf8 via "mysqli_set_charset($conn, "utf8")"</p>
+        </div>
+        <div id="besonderheiten">
+            <h2>9. Besonderheiten für die Nutzung oder Bewertung</h2>
+            <h3>Mögliche Erweiterung/TODO</h3>
+            <ul>
+                <li>Passwörter in php Funktion überprüfen lassen beim Registrieren und Constraints festlegen</li>
+                <li>Einsehen einer Liste mit Nutzern die ein bestimmtes Projekt unterstützen</li>
+                <li>Füllen der Seite mit Inhalten/Blindtexte entfernen (außerdem z.B. hinzufügen von mehr Kategorien)</li>
+                <li>Implementieren von Seitenfunktion in der Suche</li>
+            </ul>
+        </div>
+        <div id="projekt_man">
+            <h2>10. Projektmanagement-Dokumentation</h2>
+            <table>
+                <tr>
+                    <th>Tätigkeit</th>
+                    <th>Zuständige Person</th>
+                    <th>Benötigter Aufwand</th>
+                </tr>
+                <tr>
+                    <td>Erstellung Startseite inkl. Suchfunktion</td>
+                    <td>Ron Wagner</td>
+                    <td>Hoch</td>
+                </tr>
+                <tr>
+                    <td>Erstellung Header/Footer inkl. JS</td>
+                    <td>Ron Wagner</td>
+                    <td>Hoch</td>
+                </tr>
+                <tr>
+                    <td>Erstellung der Datenbank</td>
+                    <td>Paul Hentgen</td>
+                    <td>Mittel</td>
+                </tr>
+                <tr>
+                    <td>Verwaltung der Datenbank</td>
+                    <td>Paul Hentgen</td>
+                    <td>Mittel</td>
+                </tr>
+                <tr>
+                    <td>Verwaltung der Datenbank</td>
+                    <td>Ron Wagner</td>
+                    <td>Gering</td>
+                </tr>
+                <tr>
+                    <td>Erstellung der Seite Nutzerprofil inkl. anhängenden Funktionen</td>
+                    <td>Ron Wagner</td>
+                    <td>Mittel</td>
+                </tr>
+                <tr>
+                    <td>Erstellung der Seite Profil bearbeiten inkl. anhängenden Funktionen</td>
+                    <td>Ron Wagner</td>
+                    <td>Hoch</td>
+                </tr>
+                <tr>
+                    <td>Erstellen der Seite Meine Projekte</td>
+                    <td>Ron Wagner</td>
+                    <td>Mittel</td>
+                </tr>
+                <tr>
+                    <td>Erstellung der Projektseite</td>
+                    <td>Paul Hentgen</td>
+                    <td>Hoch</td>
+                </tr>
+                <tr>
+                    <td>Erstellung der Neues Projekt/ Projekt bearbeiten Seite inkl. anhängenden Funktionen</td>
+                    <td>Paul Hentgen</td>
+                    <td>Hoch</td>
+                </tr>
+                <tr>
+                    <td>Erstellung der Projekt Unterstützen Seite</td>
+                    <td>Ron Wagner</td>
+                    <td>Mittel</td>
+                </tr>
+                <tr>
+                    <td>Erstellung des Login-Modals</td>
+                    <td>Ron Wagner</td>
+                    <td>Gering</td>
+                </tr>
+                <tr>
+                    <td>Erstellung der Login/Registrieren Seite inkl. anhängenden Funktionen</td>
+                    <td>Ron Wagner</td>
+                    <td>Mittel</td>
+                </tr>
+                <tr>
+                    <td>Erstellung der Logout Funktion</td>
+                    <td>Ron Wagner</td>
+                    <td>Gering</td>
+                </tr>
+                <tr>
+                    <td>Erstellung der Seite Über Uns</td>
+                    <td>Ron Wagner</td>
+                    <td>Gering</td>
+                </tr>
+                <tr>
+                    <td>Erstellung der Seite Kontakt inkl. anhängenden Funktionen</td>
+                    <td>Ron Wagner</td>
+                    <td>Mittel</td>
+                </tr>
+                <tr>
+                    <td>Erstellung der FAQ Seite</td>
+                    <td>Ron Wagner</td>
+                    <td>Gering</td>
+                </tr>
+                <tr>
+                    <td>Erstellung des Impressums</td>
+                    <td>Ron Wagner</td>
+                    <td>Gering</td>
+                </tr>
+                <tr>
+                    <td>Erstellung der Dokumentation</td>
+                    <td>Ron Wagner</td>
+                    <td>Mittel</td>
+                </tr>
+                <tr>
+                    <td>Erweiterung der Dokumentation</td>
+                    <td>Paul Hentgen</td>
+                    <td>Gering</td>
+                </tr>
+            </table>
+        </div>
+    </div>
 
